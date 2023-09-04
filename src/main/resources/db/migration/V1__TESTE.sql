2a80fd34828f961fbbabf347a6b3321e3f8b2cdf
adelsonsljunior <junioradelsonst@gmail.com>
2023-09-04 19:17:43 -0300

    feat: migrations funvionando

--- src/main/resources/db/migration/V1__TESTE.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE usuario (
-	id serial NOT NULL,
-	nome text NOT NULL,
-	idade integer NOT NULL,
-);
-
-insert into usuario(nome, idade) values ( 'Nataniel', 29);
---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,13 @@
+-- Criação da tabela de usuários
+CREATE TABLE users (
+	id SERIAL NOT NULL PRIMARY KEY,
+	user_name TEXT NOT NULL,
+	email TEXT NOT NULL UNIQUE,
+	user_password TEXT NOT NULL,
+	url_foto TEXT
+);
+
+-- Inserção de dados na tabela de usuários
+INSERT INTO users (user_name, email, user_password) VALUES ('teste', 'teste@gmail.com', '12345678');
+INSERT INTO users (user_name, email, user_password) VALUES ('adelson', 'adelson@gmail.com', '12345678');
+INSERT INTO users (user_name, email, user_password) VALUES ('maria', 'maria@gmail.com', '12345678');
