789381ef26afc7af81a1cff986c65c58949dbaac
Adelson Júnior <junioradelsonst@gmail.com>
2023-09-07 13:51:49 +0000

    chore: class LabController

--- src/controllers/LabController.ts
+++ src/controllers/LabController.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from "express";
 
-export const hello =  async ( req: Request, res : Response ) => {
-    return res.status(200).json({hello: "Hello, Elliot!"});
-}
+export default class LabController {
 
+    public static async hello(req: Request, res: Response) {
+        return res.status(200).json({ hello: "Hello, Elliot!" });
+    }
 
-export const params = async ( req: Request, res : Response ) => {
-     const nome = req.params.nome;
+    public static async paramns(req: Request, res: Response) {
+        const nome = req.params.nome;
 
-     return res.status(200).json({nome: nome});
-}
+        return res.status(200).json({ nome: nome });
+    }
 
-export const body = async ( req: Request, res : Response ) =>{
-    const nome = req.body.nome;
+    public static async body(req: Request, res: Response) {
+        const nome = req.body.nome;
 
-    return res.status(201).json({nome: nome});
+        return res.status(201).json({ nome: nome });
+    }
+
 }
--- src/routes/LabRoutes.ts
+++ src/routes/LabRoutes.ts
@@ -1,14 +1,14 @@
 import express from "express";
 
-import { hello, params, body} from "../controllers/LabController";
+import LabController from "../controllers/LabController";
 
 const router = express.Router();
 
-router.get("/hello", hello);
+router.get("/hello", LabController.hello);
 
-router.get("/params/:nome", params)
+router.get("/params/:nome", LabController.paramns);
 
-router.post("/body", body)
+router.post("/body", LabController.body);
 
 export default router;
 
