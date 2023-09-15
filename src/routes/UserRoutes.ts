b8a50f8d7f5c4d7bf1bb6eb0c7e07f1ca0d97c19
Adelson Júnior <junioradelsonst@gmail.com>
2023-09-15 00:44:26 +0000

    feat: users crud

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -7,5 +7,9 @@ const userController = new UserController();
 const router = express.Router();
 
 router.post("/", userController.create);
+router.get("/", userController.findAll);
+router.get("/:id", userController.findById);
+router.put("/:id", userController.update)
+router.delete("/:id", userController.delete)
 
 export default router;
