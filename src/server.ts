dde8e781fff11e04bca800cac197b576ffa8f3ef
Adelson Júnior <junioradelsonst@gmail.com>
2023-09-19 14:41:03 +0000

    configurando cors

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,21 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(cors());
+app.use(cors())
+
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("origin", "*");
+    app.use(cors(options));
+    next();
+  });
 
 app.use(routes);
 
